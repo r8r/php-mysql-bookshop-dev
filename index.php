9d926dfaddf2a5a42c36b8ea7b47f126fb358172
r8r <git@r8r.us>
2019-05-11 08:34:00 +0200

    Init setup

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <title>SCM4 Book Shop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="assets/main.css" rel="stylesheet">
+
+  </head>
+<body>
+
+  <h1>SCM4 Book Shop</h1>
+
+  <script src="assets/jquery-1.11.2.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
