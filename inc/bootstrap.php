b3f43fc31d6e2fc84a2e84b717f92e5cfc391a4c
r8r <git@r8r.us>
2019-05-11 09:43:39 +0200

    Status nach 1. Pause
    + Datamanager mock

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -2,3 +2,22 @@
 declare(strict_types=1);
 ini_set('display_errors', "1");
 error_reporting(E_ALL);
+
+/**
+ *
+ * DataManager
+ * change to switch between different implementations … 'mock' | 'pdo'
+ */
+$mode = 'mock';
+switch (mb_strtolower($mode)) {
+	case 'mysqli':
+		$class = 'mysqli';
+		break;
+	case 'pdo':
+		$class = 'mysqlpdo';
+		break;
+	default:
+		$class = 'mock';
+		break;
+}
+require_once(__DIR__ . '/../lib/Data/DataManager_' . $class . '.php');
