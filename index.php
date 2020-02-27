75cb42883e21f98369a4d57aedc7065205ee5113
r8r <git@r8r.us>
2020-02-27 15:02:00 +0100

    Poor mans templating setup

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once('views/partials/header.php');
+?>
+
+<div class="page-header">
+  <h2>Welcome</h2>
+</div>
+
+<?php
+
+$foo = 1;
+$bar = "Under";
+
+print("Hello " . $bar . " World!");
+
+?>
+
+<p>Welcome to the SCM4 book shop!</p>
+
+<?php
+require_once('views/partials/footer.php');
---
+++ views/partials/footer.php
@@ -0,0 +1,23 @@
+<!--display error messages-->
+
+<!--/display error messages-->
+<div class="footer">
+	<!--display cart info-->
+	<hr />
+	<div class="col-sm-8">
+		<button class="btn btn-primary btn-xs" type="button">
+			<span class="badge">0</span> items in cart
+		</button>
+	</div>
+	<div class="col-sm-4 pull-right">
+		<p>Fri Mar 3 21:17:10 2017</p>
+	</div>
+	<!--/display cart info-->
+</div>
+</div> <!-- container -->
+
+<script src="assets/jquery-1.11.2.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
