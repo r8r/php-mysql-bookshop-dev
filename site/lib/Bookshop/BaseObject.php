dff25c7d2d162386d315bac66d0d26d13d275e21
r8r <git@r8r.us>
2022-03-24 18:09:51 +0100

    Status nach erstem Termin

---
+++ site/lib/Bookshop/BaseObject.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Bookshop;
+
+class BaseObject {
+
+}
---
+++ site/lib/Bookshop/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bookshop;
+
+class Category {
+
+	private int $id;
+	private string $name;
+
+	public function __construct(int $id, string $name) {
+		$this->id = intval($id);
+		$this->name = $name;
+	}
+
+	public function getId(): int {
+		return $this->id;
+	}
+
+	public function getName(): string {
+		return $this->name;
+	}
+
+}
